37f83b01e5e461d44ece87a02a7e4e2c6726c964
末次隆一 <sabuji117@gmail.com>
2019-04-24 09:47:40 +0900

    mymarkdown

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <Home v-if="!isLogin"></Home>
+    <Editor v-if="isLogin"></Editor>
+  </div>
+</template>
+
+<script>
+import Home from "./componemts/Hom.vue";    
+import Editor from "./componemts/Editor.vue";    
+
+export default{
+  name: "app",
+  data(){
+    return{
+      isLogin: false,
+      userData: null
+    };
+  },
+created: function(){
+  firebase.auth().onAuthStateChanged(user =>{
+    console.log(user);
+    if (user){
+      this.isLogin = true;
+      this.userData = user;
+    }else{
+      this.isLogin = false;
+      this.userData = null;
+    }
+  });
+},
+
+  components:{
+  Home: Home,
+  editor: Editor
+  }
+};
+</script>  
